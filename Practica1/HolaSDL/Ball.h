74f61beb6435fa84056dd91cdb9db546ec590e68
josepk01 <72452701+josepk01@users.noreply.github.com>
2022-11-05 20:11:59 +0100

    pelota y escape

--- Practica1/HolaSDL/Ball.h
+++ Practica1/HolaSDL/Ball.h
@@ -12,7 +12,7 @@ private:
 	Game* game = nullptr;
 public:
 	Ball() {};
-	Ball(Vector2D position, unsigned int h, unsigned w, Vector2D vel, Texture* text, Game* game) : pos(position), heigth(h), width(w), velocity(vel), tex(), game() {}
+	Ball(Vector2D position, unsigned int h, unsigned w, Vector2D vel, Texture* text, Game* game) : pos(position), heigth(h), width(w), velocity(vel), tex(text), game() {}
 	void render();
 	void update();
 
